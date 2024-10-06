f40429e6863727de6ff83e1ae54f283c0df250fb
qq1024783094 <1024783094@qq.com>
2024-10-06 19:49:19 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -15,7 +15,7 @@ export default defineUserConfig({
   // 网站描述
   description: "开源工具、效率方法、心理学探索的自我提升笔记，记录并输出一切能让自己提升的知识。",
 
-  theme: hopeTheme,
+  theme,
   // 是否开启页面预拉取，如果服务器宽带足够，可改为 true，会提升其他页面加载速度
   shouldPrefetch: true,
 
